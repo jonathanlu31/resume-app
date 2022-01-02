d77fd535deeafbeb0f16039a58c1924fb24c18c3
Jonathan Lu <jonathan.lu31@gmail.com>
2022-01-02 14:55:36 -0800

    Finish skill resume preview

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 const Skill = ({ text, rating }) => {
+  const outerBar = {
+    width: '100%',
+    height: '.6em',
+    backgroundColor: '#02315C',
+    marginBottom: '2em',
+  };
+
+  const innerBar = {
+    width: `${20 * rating}%`,
+    height: 'inherit',
+    backgroundColor: 'white',
+  };
   return (
     <div>
-      <p>
-        {text} {rating}
+      <p
+        style={{
+          margin: '1em 0 .3em 0',
+        }}
+      >
+        {text}
       </p>
+      <div style={outerBar}>
+        <div style={innerBar}></div>
+      </div>
     </div>
   );
 };
